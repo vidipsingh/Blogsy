373ae7595084e76c9ec3a38d28ed0579eba394fd
Vidip <vidip2025@gmail.com>
2024-08-10 23:50:34 +0530

    Update User Authentication with Session Storage

--- blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -1,13 +1,87 @@
+import { useContext, useRef } from "react";
 import AnimationWrapper from "../common/page-animation";
 import InputBox from "../components/input.component";
 import GoogleIcon from '../imgs/google.png'
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
+import { Toaster, toast } from 'react-hot-toast';
+import axios from 'axios';
+import { storeInSession } from "../common/session";
+import { UserContext } from "../App";
 
 const UserAuthForm = ({ type }) => {
+  
+  const authForm = useRef();
+
+  let { userAuth: { access_token }, setUserAuth } = useContext(UserContext);
+
+  console.log(access_token);
+  
+  
+
+  const userAuthThroughSever = (serverRoute, formData) => {
+      console.log(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, formData);
+      
+
+      axios.post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, formData)
+      .then(({ data }) => {
+        storeInSession("user", JSON.stringify(data))
+        
+        
+        setUserAuth(data)
+        
+        
+      })
+      .catch(({ response }) => {
+        toast.error(response.data.error)
+      })
+
+  }
+
+  const handleSubmit = (e) => {
+
+    e.preventDefault();
+    let serverRoute = type == 'sign-in' ? '/signin' : '/signup';
+
+    let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+
+    // formData
+    let form = new FormData(formElement);
+    let formData = {};
+    
+    for(let [key, value] of form.entries()){
+      formData[key] = value;
+    }
+
+    let { fullname, email, password } = formData;
+    // form validation
+    if(fullname) {
+    if (fullname.length < 3) {
+      return toast.error("Fullname must be at least 3 letters long");
+    }
+  }
+    if (!email.length) {
+      return toast.error("Enter Email");
+    }
+    if (!emailRegex.test(email)) {
+      return toast.error("Email is invalid");
+    }
+    if (!passwordRegex.test(password)) {
+      return toast.error("Password should be 6 to 20 characters long with a numeric, 1 lowercase and 1 uppercase letters");
+    }
+    
+    userAuthThroughSever(serverRoute, formData);
+
+  }
+
   return (
+    access_token ? 
+    <Navigate to='/'/> 
+    : 
     <AnimationWrapper keyValue={type}>
     <section className="h-cover flex items-center justify-center">
-      <form action="" className="w-[80%] max-w-[400px] ">
+      <Toaster/>
+      <form id="formElement" action="" className="w-[80%] max-w-[400px] ">
         <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
           {type == "sign-in" ? "Welcome back" : "Join us today"}
         </h1>
@@ -37,7 +111,7 @@ const UserAuthForm = ({ type }) => {
           icon=" fi-rr-key"
         />
 
-        <button className="btn-dark center mt-14" type="submit">
+        <button className="btn-dark center mt-14" type="submit" onClick={handleSubmit} >
             { type.replace("-", " ") }
         </button>
 
