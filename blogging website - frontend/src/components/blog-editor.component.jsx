590c2d56df6b7bf6453818a2393e9404c011be99
Vidip <vidip2025@gmail.com>
2024-08-15 17:20:48 +0530

    Update Publish Form Page
    Have to solve the same image upload problem from aws like server

--- blogging website - frontend/src/components/blog-editor.component.jsx
+++ blogging website - frontend/src/components/blog-editor.component.jsx
@@ -16,7 +16,7 @@ const BlogEditor = () => {
     useEffect(() => {
       setTextEditor(new EditorJS({
         holderId: "textEditor",
-        data: '',
+        data: content,
         tools: tools,
         placeholder: "Write a masterpiece!"
       }))
@@ -128,7 +128,7 @@ const BlogEditor = () => {
                         </label>
                     </div>
 
-                    <textarea  placeholder="Blog Title" className="text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40 " onKeyDown={handleTitleKeyDown} onChange={handleTitleChange} >
+                    <textarea defaultValue={title} placeholder="Blog Title" className="text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40 " onKeyDown={handleTitleKeyDown} onChange={handleTitleChange} >
 
                     </textarea>
 
