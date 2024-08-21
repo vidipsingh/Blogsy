42c978d61b09c5b67827249550425b2046466185
Vidip <vidip2025@gmail.com>
2024-08-21 23:39:26 +0530

    Update Home Page
    Trending blog section updated

--- blogging website - frontend/src/pages/home.page.jsx
+++ blogging website - frontend/src/pages/home.page.jsx
@@ -4,10 +4,12 @@ import InPageNavigation from "../components/inpage-navigation.component";
 import { useEffect, useState } from "react";
 import Loader from "../components/loader.component";
 import BlogPostCard from "../components/blog-post.component";
+import MinimalBlogPost from "../components/nobanner-blog-post.component";
 
 const HomePage = () => {
 
     let [ blogs, setBlog ] = useState(null);
+    const [trendingBlogs, setTrendingBlogs] = useState(null);
 
     const fetchLatestBlogs = () => {
         axios.get('http://localhost:3000/latest-blogs')
@@ -21,8 +23,22 @@ const HomePage = () => {
         })
     }
 
+    const fetchTrendingBlogs = () => {
+        axios.get('http://localhost:3000/latest-blogs')
+        .then(({ data }) => {
+            setTrendingBlogs(data.blogs);
+            
+        })
+        .catch(err => {
+            console.log(err);
+            
+        })
+    }
+
+
     useEffect(() => {
         fetchLatestBlogs();
+        fetchTrendingBlogs();
     }, [])
 
     return (
@@ -43,14 +59,37 @@ const HomePage = () => {
                             }
                         </>
 
-                        <h1>Trending blogs here</h1>
+                        {
+                             trendingBlogs == null ? <Loader /> : trendingBlogs.map((blog, i) => {
+                                return <AnimationWrapper transition={{ duration: 1, delay: i*.1 }} key={i} >
+                                    <MinimalBlogPost blog={blog} index={i} />
+                                </AnimationWrapper>
+                            })
+                        }
 
                     </InPageNavigation>
 
                 </div>
 
                 {/* for filters and trending blogs */}
-                <div>
+                <div className="min-w-[40%] lg:min-w-[400px] max-w-min border-l border-grey pl-8 pt-3 max-md:hidden">
+                            
+                        <div className="flex flex-col gap-10">
+                            <h1 className="font-medium text-xl mb-8">Stories from all interests</h1>
+                        </div>
+
+                        <div>
+                            <h1 className="font-medium text-xl mb-8">Trending <i className="fi fi-rr-arrow-trend-up"></i></h1>
+
+                            {
+                             trendingBlogs == null ? <Loader /> : trendingBlogs.map((blog, i) => {
+                                return <AnimationWrapper transition={{ duration: 1, delay: i*.1 }} key={i} >
+                                    <MinimalBlogPost blog={blog} index={i} />
+                                </AnimationWrapper>
+                            })
+                            }
+
+                        </div>
 
                 </div>
             </section>
--- server/server.js
+++ server/server.js
@@ -301,6 +301,22 @@ server.post("/signup", async (req, res) => {
 
   })
 
+  server.get('/trending-blogs', (req, res) => {
+
+    Blog.find({ draft: false })
+    .populate("author", "personal_info.profile_img personal_info.username personal_info.fullname -_id ")
+    .sort({ "activity.total_reads": -1, "activity.total_likes": -1, "publishedAt": -1 })
+    .select("blog_id title publishedAt -_id")
+    .limit(5)
+    .then(blogs => {
+      return res.status(200).json({ blogs })
+    })
+    .catch(err => {
+      return res.status(500).json({ error: err.message })
+    })
+
+  })
+
   server.post('/create-blog', verifyJWT, (req,res) => {
 
     let authId = req.user;
