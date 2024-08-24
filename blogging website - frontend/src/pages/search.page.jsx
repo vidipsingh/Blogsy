02346e0a9231d63574d4273e8cb00cd658fd230d
Vidip <vidip2025@gmail.com>
2024-08-25 01:27:02 +0530

    Update search User option in Home Page

--- blogging website - frontend/src/pages/search.page.jsx
+++ blogging website - frontend/src/pages/search.page.jsx
@@ -8,12 +8,14 @@ import NoDataMessage from "../components/nodata.component";
 import LoadMoreDataBtn from "../components/load-more.component";
 import axios from "axios";
 import { filterPaginationData } from "../common/filter-pagination-data";
+import UserCard from "../components/usercard.component";
 
 const SearchPage = () => {
 
     let { query } = useParams();
 
     const [blogs, setBlog] = useState(null);
+    let [ users, setUsers ] = useState(null);
 
     const searchBlogs = ({ page = 1, create_new_arr = false }) => {
 
@@ -43,20 +45,47 @@ const SearchPage = () => {
         })
     }
 
+    const fetchUsers = () => {
+        axios.post('http://localhost:3000/search-users', { query })
+        .then(({ data: { users } }) => {
+            setUsers(users);
+        })
+    }
+
     useEffect(() => {
 
         resetState();
         searchBlogs({ page: 1, create_new_arr: true });
+        fetchUsers();
+
     }, [query])
 
     const resetState = () => {
         setBlog(null);
+        setUsers(null);
+    }
+
+    const UserCardWrapper = () => {
+        return (
+            <>
+                {
+                    users == null ? <Loader /> :
+                        users.length ? 
+                            users.map((user, i) => {
+                                return <AnimationWrapper key={i} transition={{ duration: 1, delay: i * 0.08 }} >
+                                    <UserCard user={user} />
+                                </AnimationWrapper> 
+                            })
+                        : <NoDataMessage message="No User Found" />
+                }
+            </>
+        )
     }
 
     return (
         <section className="h-cover flex justify-center gap-10">
             <div className="w-full">
-                <InPageNavigation routes={[`Searh Results form "${query}"`, "Account Matched"]} defaultHidden={["Account Matched"]}>
+                <InPageNavigation routes={[`Searh Results form "${query}"`, "Accounts Matched"]} defaultHidden={["Accounts Matched"]}>
 
                     <>
                             {
@@ -73,7 +102,17 @@ const SearchPage = () => {
                             <LoadMoreDataBtn state={blogs} fetchDataFn={searchBlogs} />
                     </>
 
+                    <UserCardWrapper />
+
                 </InPageNavigation>
+            </div>
+
+            <div className="min-w-[40%] lg:min-w-[350px] max-w-min border-l border-grey pl-8 pt-3 max-md:hidden ">
+
+                <h1 className="font-medium text-xl mb-8">User related to search <i className="fi fi-rr-user mt-1"></i></h1>
+
+                <UserCardWrapper />
+
             </div>
         </section>
     )
--- server/server.js
+++ server/server.js
@@ -385,6 +385,21 @@ server.post("/signup", async (req, res) => {
     })
   })
 
+  server.post('/search-users', (req, res) => {
+    let { query } = req.body;
+
+    User.find({ "personal_info.username": new RegExp(query, 'i') })
+    .limit(50)
+    .select("personal_info.fullname personal_info.username personal_info.profile_img -_id")
+    .then(users => {
+      return res.status(200).json({ users })
+    })
+    .catch(err => {
+      return res.status(500).json({ error: err.message })
+    })
+
+  })
+
   server.post('/create-blog', verifyJWT, (req,res) => {
 
     let authId = req.user;
