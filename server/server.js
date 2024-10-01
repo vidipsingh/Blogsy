8a8230fea54397df0d9ab7562d4668ed5d880b73
Vidip Singh <vidip2025@gmail.com>
2024-10-01 23:38:12 +0530

    Update Firebase credentials

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import { nanoid } from 'nanoid';
 import jwt from 'jsonwebtoken';
 import cors from 'cors';
 import admin from 'firebase-admin';
-import serviceAccountKey from './blogsy-9935d-firebase-adminsdk-uaa24-0029c5d820.json' assert {type: "json"} ;
+import serviceAccountKey from './blogsy-9935d-firebase-adminsdk-uaa24-df21554d8a.json' assert {type: "json"} ;
 import { getAuth } from 'firebase-admin/auth';
 // import aws from 'aws-sdk';
 
