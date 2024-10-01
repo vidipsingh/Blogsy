88f4fca2cb4182c69886b1ea8ec9fb2b30b42f7e
Vidip Singh <vidip2025@gmail.com>
2024-10-02 00:00:00 +0530

    Moved Firebase credentials to .env file

--- server/server.js
+++ server/server.js
@@ -6,9 +6,23 @@ import { nanoid } from 'nanoid';
 import jwt from 'jsonwebtoken';
 import cors from 'cors';
 import admin from 'firebase-admin';
-import serviceAccountKey from './blogsy-9935d-firebase-adminsdk-uaa24-df21554d8a.json' assert {type: "json"} ;
+// import serviceAccountKey from './blogsy-9935d-firebase-adminsdk-uaa24-df21554d8a.json' assert {type: "json"} ;
 import { getAuth } from 'firebase-admin/auth';
 // import aws from 'aws-sdk';
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
 
 // Schemas
@@ -21,7 +35,7 @@ const server = express();
 let PORT = 3000;
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccountKey)
+    credential: admin.credential.cert(serviceAccount)
 })
 
 let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
